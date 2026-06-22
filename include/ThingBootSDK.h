108978975536ec904e828c358ab4855fca6f91b2
SDK Builder <sdk@thingboot.com>
2026-06-22 09:16:28 +0800

    Release v1.0.1

--- include/ThingBootSDK.h
+++ include/ThingBootSDK.h
@@ -9,14 +9,13 @@
 // 版本号由 pack_lib.py 自动同步 VERSION 文件
 #define THINGBOOT_SDK_VERSION "1.0.1"
 
-#if defined(ESP8266) || defined(ESP32)
 #include <functional>
 #define TB_CALLBACK_EVENT std::function<void(SYS_STATE, bool)> tb_event_callback
 #define TB_CALLBACK_CONFIG std::function<void(const char*, String)> tb_config_callback
-#else
-#define TB_CALLBACK_EVENT void (*tb_event_callback)(SYS_STATE, bool)
-#define TB_CALLBACK_CONFIG void (*tb_config_callback)(const char*, String)
-#endif
+
+#define TB_CALLBACK_TIMER10MS std::function<void()> tb_config_callback
+#define TB_CALLBACK_TIMER100MS std::function<void()> tb_config_callback
+#define TB_CALLBACK_TIMER1000MS std::function<void()> tb_config_callback
 
 class ThingBootDevice {
 private:
